e8d0ccbd127ae792362181616cbc91703796cb66
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-10-18 21:16:08 -0400

    Created Nut model.

---
+++ app/models/squarrel/nut.rb
@@ -0,0 +1,13 @@
+module Squarrel
+  # The 'nut' (SQRL nonce) that is created to authenticate a user.
+  class Nut
+    def self.generate
+    end
+
+    private
+
+    # Creates a symmetric key that will be used to encrypt the nut.
+    def self.key
+    end
+  end
+end
--- squarrel.gemspec
+++ squarrel.gemspec
@@ -22,4 +22,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency "rspec-rails"
   s.add_development_dependency "capybara"
   s.add_development_dependency "factory_girl_rails"
+
+  s.add_runtime_dependency "rbnacl"
 end
