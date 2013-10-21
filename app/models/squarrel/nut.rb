92752d3a95f11267850c8bae095d2511260b995a
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-10-21 18:06:24 -0400

    Refactored Nut class.

--- app/models/squarrel/nut.rb
+++ app/models/squarrel/nut.rb
@@ -7,17 +7,21 @@ module Squarrel
     # Nuts expire 5 minutes after they are created.
     EXPIRATION_SECONDS = 300
 
-    # Generates a new encrypted nut for a specific IP address.
+    #  Constructs a new nut for a specific user/IP.
     def self.generate(ip)
+      timestamp = Time.now.to_i
+      
       payload = {
         "ip" => ip,
-        "ts" => Time.now.to_i }
+        "ts" => timestamp }
 
       message = JSON.generate(payload)
-
       enc = self.encrypt(message)
 
-      "#{enc[:message].unpack("h*")[0]}.#{enc[:nonce].unpack("h*")[0]}"
+      nut = enc[:message]
+      nonce = enc[:nonce]
+
+      new(ip, timestamp, nut, nonce)
     end
 
     # Validates a nut and signature provided by the user.
@@ -59,27 +63,46 @@ module Squarrel
       begin
         # Decrypt the provided nut.
         split = nut.split(".")
-        nut = [split[0]].pack("h*")
-        nonce = [split[1]].pack("h*")
+        nut = [split[0]].pack("H*")
+        nonce = [split[1]].pack("H*")
 
-        nut = decrypt(nut, nonce)
-        nut = JSON.parse(nut)
+        message = decrypt(nut, nonce)
+        message = JSON.parse(message)
       rescue
         raise Squarrel::BadNut.new("Received an invalid nonce.")
       end
 
       # Check the nut timestamp.
-      delta = Time.now.to_i - nut["ts"].to_i
+      auth_time = Time.now.to_i
+      timestamp = message["ts"].to_i
+      delta = auth_time - timestamp
       raise Squarrel::BadNut.new("Nonce has expired.") if delta > EXPIRATION_SECONDS
 
       # If "enforce" was specified, check the IP.
+      orig_ip = message["ip"]
       if sqrl_options.include? "enforce"
-        orig_ip = nut["ip"]
         raise Squarrel::BadNut.new("IP address mismatch.") if ip != orig_ip
       end
       
-      # Record and return the successful authentication.
-      Authentication.authenticate(ip, orig_ip, nut, sqrlkey)
+      new(orig_ip, timestamp, nut, nonce, ip, auth_time, sqrlkey)
+    end
+
+    attr_reader :auth_ip, :sqrl_key, :ip, :nut, :nonce, :timestamp, :auth_time
+
+    private_class_method :new
+
+    def initialize(ip, timestamp, nut, nonce, auth_ip = nil, auth_time = nil, sqrl_key = nil)
+      @ip = ip
+      @timestamp = timestamp
+      @nut = nut
+      @nonce = nonce
+      @auth_ip = auth_ip
+      @auth_time = auth_time
+      @sqrl_key = sqrl_key
+    end
+
+    def to_s
+      "#{nut.unpack("H*")[0]}.#{nonce.unpack("H*")[0]}"
     end
 
     private
--- spec/models/nut_spec.rb
+++ spec/models/nut_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 describe Squarrel::Nut do
   describe "generating a nut" do
     it "should return a message/nonce pair" do
-      expect(Squarrel::Nut.generate("127.0.0.1")).to match(/[0-9a-z]{32,}\.[0-9a-z]{32,}/i)
+      expect(Squarrel::Nut.generate("127.0.0.1").to_s)
+      .to match(/[0-9a-z]{32,}\.[0-9a-z]{32,}/i)
     end
 
     it "should return a different nonce every time" do
@@ -14,7 +15,7 @@ describe Squarrel::Nut do
 
       expect(nuts.length).to eq(10)
 
-      nonces = nuts.map { |n| n.split(".")[0] }
+      nonces = nuts.map { |n| n.to_s.split(".")[0] }
       expect(nonces.length).to eq(10)
       expect(nonces.uniq.length).to eq(10)
     end
@@ -25,7 +26,7 @@ describe Squarrel::Nut do
     let(:ip) { "127.0.0.1" }
 
     # New nut generated for the user upon request.
-    let!(:nut) { Squarrel::Nut.generate(ip) }
+    let!(:nut) { Squarrel::Nut.generate(ip).to_s }
 
     # Generate a pub/sec keypair for the fake user.
     let(:key) { RbNaCl::SigningKey.generate }
@@ -53,9 +54,9 @@ describe Squarrel::Nut do
         split = nut.split(".")
         nonce = split[1]
         nonce[0] = nonce[0] == "0" ? "1" : "0"
-        nut = split.join(".")
+        altered = split.join(".")
 
-        uri = sqrl_uri(nut, 1, pub_key)
+        uri = sqrl_uri(altered, 1, pub_key)
         expect {
           Squarrel::Nut.validate(ip, uri, sign(uri))
         }.to raise_error Squarrel::BadNut, /nonce/
@@ -67,9 +68,9 @@ describe Squarrel::Nut do
         split = nut.split(".")
         val = split[0]
         val[0] = val[0] == "0" ? "1" : "0"
-        nut = split.join(".")
+        altered = split.join(".")
 
-        uri = sqrl_uri(nut, 1, pub_key)
+        uri = sqrl_uri(altered, 1, pub_key)
         expect {
           Squarrel::Nut.validate(ip, uri, sign(uri))
         }.to raise_error Squarrel::BadNut, /nonce/
@@ -123,11 +124,19 @@ describe Squarrel::Nut do
         result = Squarrel::Nut.validate(ip, post_uri, sign(post_uri))
 
         expect(result).not_to be_nil
-        expect(result).to be_a Squarrel::Authentication
-        expect(result.nut).to eq(nut)
+        expect(result).to be_a Squarrel::Nut
+        expect(result.to_s).to eq(nut)
+
         expect(result.ip).to eq(ip)
-        expect(result.orig_ip).to eq(up)
-        expect(result.user).not_to be_nil
+        expect(result.timestamp).not_to be_nil
+        expect(result.timestamp).to be <= Time.now.to_i
+        expect(result.nut.unpack("H*")[0]).to eq(nut.split(".")[0])
+        expect(result.nonce.unpack("H*")[0]).to eq(nut.split(".")[1])
+        expect(result.auth_ip).to eq(ip)
+        expect(result.auth_time).not_to be_nil
+        expect(result.auth_time).to be <= Time.now.to_i
+        expect(result.sqrl_key).not_to be_nil
+        expect(result.sqrl_key).to eq(pub_key)
       end
     end
   end
