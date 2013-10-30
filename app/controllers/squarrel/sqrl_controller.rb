ff8b0ac97c9a86a59f99ce4d3ecebe4524d0c85e
Nathan Clark <Nathan.Clark@tokenshift.com>
2013-10-29 21:02:44 -0400

    Added basic login form.

--- app/controllers/squarrel/sqrl_controller.rb
+++ app/controllers/squarrel/sqrl_controller.rb
@@ -8,6 +8,8 @@ module Squarrel
 
     # Renders a simple, unstyled login form.
     def form
+      @nut = Nut.generate(request.remote_ip)
+      @uri = callback_url(protocol: "sqrl", nut: @nut.to_s)
     end
     
     # Callback for an authentication app.
